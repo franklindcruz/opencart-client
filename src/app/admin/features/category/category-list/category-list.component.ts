37371f410ef1e65b77cfa711842cc4fe1a3b9cbf
franklindcruz <franklinroyson213191@gmail.com>
2025-05-09 15:27:08 +0530

    Creating and Using Services From a Component

--- src/app/admin/features/category/category-list/category-list.component.ts
+++ src/app/admin/features/category/category-list/category-list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import CategoryViewDto from '../../../../shared/dtos/category-view-dto';
-import Result from '../../../../shared/types/result';
 import { HttpClient } from '@angular/common/http';
-
+import { Component, OnInit } from '@angular/core';
+import Result from '../../../../shared/types/result';
+import CategoryViewDto from '../../../../shared/dtos/category-view-dto';
+import { CategoryService } from '../../../../shared/services/category.service';
 @Component({
   selector: 'app-category-list',
   standalone: false,
@@ -10,16 +10,13 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './category-list.component.css',
 })
 export class CategoryListComponent implements OnInit {
-  
   categories?: CategoryViewDto[];
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
-    this.httpClient
-      .get<Result<CategoryViewDto[]>>('http://localhost:5150/Category')
-      .subscribe((res) => {
-        this.categories = res.data;
-      });
+    this.categoryService
+      .getAll()
+      .subscribe((result) => (this.categories = result.data));
   }
 }
--- src/app/shared/services/category.service.ts
+++ src/app/shared/services/category.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import Result from '../types/result';
+import CategoryViewDto from '../dtos/category-view-dto';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getAll() {
+    return this.httpClient.get<Result<CategoryViewDto[]>>(
+      'http://localhost:5150/Category'
+    );
+  }
 }
