b422f3bf81540593e13ed4f0fc8e0bd73f1bd310
franklindcruz <franklinroyson213191@gmail.com>
2025-05-09 16:26:31 +0530

    Binding ngModel to template variables

--- src/app/admin/features/category/category-create/category-create.component.ts
+++ src/app/admin/features/category/category-create/category-create.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import CategoryCreateDto from '../../../../shared/dtos/category-create-dto';
+import { NgModel } from '@angular/forms';
 
 
 @Component({
@@ -9,8 +10,12 @@ import CategoryCreateDto from '../../../../shared/dtos/category-create-dto';
   styleUrl: './category-create.component.css',
 })
 export class CategoryCreateComponent {
-  model: CategoryCreateDto = {
-    name: '',
-    description: null,
-  };
+    model: CategoryCreateDto = {
+        name: '',
+        description: null
+    }
+
+    display(name: NgModel) {
+        console.log(name);
+    }
 }
