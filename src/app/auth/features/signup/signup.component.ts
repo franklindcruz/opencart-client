684f5ffd93b2eb250fb085d021a2d54789a50135
franklindcruz <franklinroyson213191@gmail.com>
2025-05-12 15:31:41 +0530

    Handling Form Validation

--- src/app/auth/features/signup/signup.component.ts
+++ src/app/auth/features/signup/signup.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-signup',
   standalone: false,
@@ -10,11 +15,15 @@ export class SignupComponent {
   signupForm: FormGroup;
   constructor(private fb: FormBuilder) {
     this.signupForm = fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(25),
+      ]),
+      lastName: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+      confirmPassword: new FormControl(''),
     });
   }
 
