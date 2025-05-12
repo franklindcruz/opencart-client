a9141ecc6b7cc306f372b3a710486075954167f3
franklindcruz <franklinroyson213191@gmail.com>
2025-05-12 14:40:44 +0530

    Created Auth Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,10 @@ const routes: Routes = [
     loadChildren: () =>
       import('./admin/admin.module').then((m) => m.AdminModule),
   },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
 ];
 
 @NgModule({
